8ccd3c254b90fda6b09a03b75a2774824b8c78db
bill <bocquet.pierre@gmail.com>
2018-04-29 23:42:09 +0200

    first commit

---
+++ src/main/java/com/payeat/microservice/eureka/naming/server/microserviceeurekanamingserver/MicroserviceEurekaNamingServerApplication.java
@@ -0,0 +1,14 @@
+package com.payeat.microservice.eureka.naming.server.microserviceeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEurekaNamingServerApplication.class, args);
+	}
+}
